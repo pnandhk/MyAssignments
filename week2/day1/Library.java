af1267f112c9b419b45cb3800fd9ce7f23058ace
pnandhk <86149172+pnandhk@users.noreply.github.com>
2024-12-31 11:06:24 +0530

    Add files via upload

---
+++ week2/day1/Library.java
@@ -0,0 +1,30 @@
+package week2.day1;
+
+public class Library {
+	
+	
+	public String addBook(String bookTitle) {
+		System.out.println(bookTitle+" "+"Book added Successfully"); 
+		return bookTitle;
+		
+		
+	}
+	
+	public void issueBook() {
+		System.out.println("Book issued Successfully"); 
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	      Library book1 = new Library();
+	      book1.addBook("Harry Potter");
+	      
+	      Library book2 = new Library();
+	      book2.issueBook();
+	      
+	      
+
+	}
+
+}
---
+++ week2/day1/LibraryManagement2.java
@@ -0,0 +1,15 @@
+package week2.day1;
+
+public class LibraryManagement2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Library book1 = new Library();
+		book1.addBook("Ponniyin Selvan");
+		
+		Library book2 = new Library();
+		book2.issueBook();
+	}
+
+}
